0aacb6943294b3ed1b87de926702cb651d83ad69
Nix <yiunixon@gmail.com>
2018-07-09 18:17:50 -0700

    write group anagrams

--- lru_cache.js
+++ lru_cache.js
@@ -17,8 +17,9 @@ LRUCache.prototype.get = function (key) {
 };
 
 LRUCache.prototype.put = function (key, value) {
-  if (!this.capacity) {
+  if (this.capacity === 0) {
     this.remove(this.linkedList.head.next);
+    // was removing wrong key;
   }
   const newNode = new Node(value);
   this.append(newNode);
@@ -55,3 +56,14 @@ var LinkedList = function () {
   this.head.next = this.tail;
   this.tail.prev = this.head;
 };
+
+let cache = new LRUCache(2);
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+// cache.put(4, 4);    // evicts key 1
+// cache.get(1);       // returns -1 (not found)
+// cache.get(3);       // returns 3
+// cache.get(4);
