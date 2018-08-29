f568d15189d735842fcfaa6a32f622fd71853f1c
Nix <yiunixon@gmail.com>
2018-08-28 21:42:51 -0700

    iterative in order traversal

--- inOrderTraversal.js
+++ inOrderTraversal.js
@@ -1,15 +1,31 @@
-var inorderTraversal = function (root, res = []) {
-  if (root === null) { return res }
+// var inorderTraversal = function (root, res = []) {
+//   if (root === null) { return res }
 
-  if (root.left) {
-    inorderTraversal(root.left, res)
+//   if (root.left) {
+//     inorderTraversal(root.left, res)
+//   }
+
+//   res.push(root.val)
+
+//   if (root.right) {
+//     inorderTraversal(root.right, res)
+//   }
+
+//   return res
+// };
+var inorderTraversal = function (root) {
+  let res = []
+  let stack = []
+  let cur = root
+
+  while (cur || stack.length > 0) {
+    while (cur) {
+      stack.push(cur)
+      cur = cur.left
+    }
+    cur = stack.pop()
+    res.push(cur.val)
+    cur = cur.right
   }
-
-  res.push(root.val)
-
-  if (root.right) {
-    inorderTraversal(root.right, res)
-  }
-
   return res
 };
